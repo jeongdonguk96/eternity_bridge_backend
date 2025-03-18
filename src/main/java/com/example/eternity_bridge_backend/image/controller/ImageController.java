4e482b8f7d71c79df76701f314fbf2542cb686d8
jeongdonguk <foodsmith96@naver.com>
2025-03-18 11:13:29 +0900

    REFACTOR - 이미지 생성 API 시 ImageDomain 파라미터 추가

--- src/main/java/com/example/eternity_bridge_backend/image/controller/ImageController.java
+++ src/main/java/com/example/eternity_bridge_backend/image/controller/ImageController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -22,7 +23,10 @@ public class ImageController {
 
     // S3에 이미지 파일을 업로드하고 DB에 이미지 데이터를 생성한다.
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public String createImage(@RequestPart(name = "file") MultipartFile file, ImageDomain domain) throws IOException {
+    public String createImage(
+            @RequestPart(name = "file") MultipartFile file,
+            @RequestParam(name = "domain") ImageDomain domain
+    ) throws IOException {
         Long memberId = 1L;
         return imageService.createImage(file, domain, memberId);
     }
