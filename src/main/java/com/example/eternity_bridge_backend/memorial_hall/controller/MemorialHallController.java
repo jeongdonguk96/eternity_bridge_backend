ff3e21c291090abfc7afbe55a68c614b99103390
jeongdonguk <foodsmith96@naver.com>
2025-03-18 10:35:00 +0900

    REFACTOR - API path에 prefix로 "/api" 지정

--- src/main/java/com/example/eternity_bridge_backend/memorial_hall/controller/MemorialHallController.java
+++ src/main/java/com/example/eternity_bridge_backend/memorial_hall/controller/MemorialHallController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/memorials")
+@RequestMapping("/api/memorials")
 @RequiredArgsConstructor
 public class MemorialHallController {
 
